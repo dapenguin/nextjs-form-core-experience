a6cc68d41657049943062f3659d2e18548d06f06
Anthony Jeffery <anthonyjeffery@notonthehighstreet.com>
2020-04-03 12:30:49 +0100

    feat: form for creating transformers. If a POST request comes from a normal HTML form, then redirect to a page. Otherwise, we'll treat it as a client-side fetch and return a created status and let the client side redirect using next/router.

--- pages/api/transformers.jsx
+++ pages/api/transformers.jsx
@@ -1,28 +1,55 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const transformersData = [
     {
-        id: 1,
+        id: uuidv4(),
         name: 'Optimus Prime',
         faction: 'Autobots'
     },
     {
-        id: 2,
+        id: uuidv4(),
         name: 'Jazz',
         faction: 'Autobots'
     },
     {
-        id: 3,
+        id: uuidv4(),
         name: 'Megatron',
         faction: 'Decepticons'
     },
     {
-        id: 4,
+        id: uuidv4(),
         name: 'Soundwave',
         faction: 'Decepticons'
     }
 ];
 
+const get = (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(transformersData));
+};
+
+const post = (req, res) => {
+    const { name, faction } = req.body;
+
+    transformersData.push({ name, faction, id: uuidv4() })
+
+    /* If the data was submitted via a form, then redirect to a page, otherwise
+     * return with a 201 Created status.
+     */
+    if (req.headers['content-type'].includes('application/x-www-form-urlencoded')) {
+        res.writeHead(302, { Location: '/' })
+    } else {
+        res.statusCode = 201;
+    }
+
+    res.end();
+};
+
 export default (req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(transformersData))
+    if (req.method === 'POST') {
+        post(req, res);
+    } else {
+        get(req, res);
+    }
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Router from 'next/router';
+import serialize from 'form-serialize';
 import 'isomorphic-fetch';
 
 export default class Home extends Component {
+    constructor() {
+        super();
+
+        this.formRef = React.createRef();
+    }
+
     static async getInitialProps() {
         try {
             const response = await fetch('http://localhost:8090/api/transformers');
             const transformers = await response.json();
             return { transformers };
+        } catch(error) {
+            console.log({error});
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const formData = serialize(this.formRef.current, { hash: true });
+        try {
+            const response = await fetch('http://localhost:8090/api/transformers', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                },
+            });
+
+            if (!response.ok) {
+                throw 'Error forging transformer.'
+            }
+
+            Router.push('/');
         } catch(error) {
             console.log({error});
         }
@@ -23,6 +53,19 @@ export default class Home extends Component {
                         {name}, {faction}
                     </p>
                 ))}
+                <h2>Forge a new transformer</h2>
+                <form action="/api/transformers" method="POST" onSubmit={this.handleSubmit} ref={this.formRef}>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" autoComplete="off" />
+                    <br />
+                    <label htmlFor="faction">Faction:</label>
+                    <select id="faction" name="faction">
+                        <option value="Autobots">Autobots</option>
+                        <option value="Decepticons">Decepticons</option>
+                    </select>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
             </>
         );
     }
